74d0d3200b0549ee4fa560e7e8432cc5a7b33f4b
pwnwriter <hey@pwnwriter.xyz>
2023-11-20 10:58:06 +0545

    feat(pkg-repo): load repo url from env else use @bytehunt/seren-pkgs

--- src/engine/data_parser.rs
+++ src/engine/data_parser.rs
@@ -3,28 +3,38 @@ use anyhow::{anyhow, Result};
 use spinoff::{spinners, Color, Spinner};
 
 pub async fn fetch_and_process_toml(pkg_name: &str) -> Result<PackageInfo> {
-    const RAW_URL: &str = "https://raw.githubusercontent.com/bytehunt/seren-pkgs/main/data/";
+    let pkg_toml_file_url = build_package_toml_url(pkg_name)?;
 
-    let pkg_toml_file = format!("{}.toml", pkg_name);
-    let pkg_toml_file_url = format!("{}{}", RAW_URL, pkg_toml_file);
-    let _ = Spinner::new(spinners::Dots, "Fetching package info... ", Color::Green); 
-    // TODO: Implement some fancy style for spinner
-    let response = match reqwest::get(&pkg_toml_file_url).await {
-        Ok(res) => res,
-        Err(e) => return Err(anyhow!("Failed to fetch package: {}", e)),
-    };
+    let _ = Spinner::new(spinners::Dots, "Fetching package info... ", Color::Green);
+    let response = fetch_package_info(&pkg_toml_file_url).await?;
+
     println!();
+
+    parse_package_response(response).await
+}
+
+// Read repo url from the envirovnment else use default one @bytehunt/seren-pkgs
+fn build_package_toml_url(pkg_name: &str) -> Result<String> {
+    let repo_url: String =
+        std::env::var("SEREN_REPO_URL").unwrap_or("bytehunt/seren-pkgs".to_string());
+    let raw_url = format!("https://raw.githubusercontent.com/{}/main/data/", repo_url);
+    let pkg_toml_file = format!("{}.toml", pkg_name);
+    Ok(format!("{}{}", raw_url, pkg_toml_file))
+}
+
+async fn fetch_package_info(url: &str) -> Result<reqwest::Response> {
+    match reqwest::get(url).await {
+        Ok(res) => Ok(res),
+        Err(e) => Err(anyhow!("Failed to fetch package: {}", e)),
+    }
+}
+
+async fn parse_package_response(response: reqwest::Response) -> Result<PackageInfo> {
     match response.status() {
         reqwest::StatusCode::OK => {
-            let toml_text = match response.text().await {
-                Ok(text) => text,
-                Err(e) => return Err(anyhow!("Failed to parse response {}", e)),
-            };
-
+            let toml_text = response.text().await?;
             let parsed_toml: PackageInfo = toml::from_str(&toml_text)?;
-
             println!("{:?}", parsed_toml);
-
             Ok(parsed_toml)
         }
         _ => Err(anyhow!("No such package found")),
