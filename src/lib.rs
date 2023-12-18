ebe461e24f4bfbec264b84507f567904a23bc55b
pwnwriter <hey@pwnwriter.xyz>
2023-12-18 09:22:12 +0545

    chore(lib,dox): fixed license statements

--- src/lib.rs
+++ src/lib.rs
@@ -14,4 +14,4 @@ pub mod engine;
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+// furnished to do so.
